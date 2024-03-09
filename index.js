28642b6168c840ba1f6b7fe477d9fbee9f8e7e10
coli <155027158+discord-collaborative-adventure@users.noreply.github.com>
2024-03-09 15:44:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.login(process.env.DISCORD_TOKEN);
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'ping') {
     msg.reply('Pong');
   }
 });
-
-client.login('MTIxNjAzMjkxNDIyNDA1ODM4OA.GPtTuc.4jvoPmccvlS7VIg9H44f92TCKl57txH4aCw-tw');
